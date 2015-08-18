ff5222c69629f92b60a116715173708b71c24e7f
MrHash <mr.hash@gmail.com>
2015-08-19 01:11:14 +0200

    Generated project from honeybee-agavi-cmf-project

--- README.md
+++ README.md
@@ -1,1 +1,145 @@
-# Honeybee-Agavi Demo
+# honeybee-agavi-cmf-demo
+
+### Development setup
+
+This repository holds a `honeybee-agavi-cmf-demo` template application which you can use to create your own projects.
+
+#### Checking the prerequisites
+
+* VirtualBox: https://www.virtualbox.org/wiki/Downloads
+* VirtualBox Extension Pack: https://www.virtualbox.org/wiki/Download_Old_Builds_4_2
+    * either add via Menu: `VirtualBox` -> `File` -> `Preferences` -> `Extensions`
+    * or doubleclick the extension pack file after downloading
+* Vagrant: http://downloads.vagrantup.com/
+* Git: https://git-scm.com
+
+##### Optional devops dependencies
+
+* Ruby: https://www.ruby-lang.org/de/
+* Puppet: `gem install puppet`
+* Librarian: `gem install librarian-puppet`
+
+#### Setting up `ssh` and `git`
+
+You can configure `git` manually inside your VM or you can preconfigure environment variables to pass into the VM as follows:
+
+* Add `GIT_*` environment-vars in `~/.bashrc` or `~/.bash_profile`:
+```shell
+PATH=$PATH:/opt/vagrant/bin
+export GIT_AUTHOR_NAME="User Name"
+export GIT_AUTHOR_EMAIL="your@email.de"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+```
+
+* Enable `ssh-agent` and environment-vars forwarding in `~/.ssh/config`:
+```shell
+Host *
+ForwardAgent yes
+SendEnv LANG LC_* GIT_*
+```
+
+#### Initially setting up the project
+
+* To create a new project and VM from this template using `composer`:
+```shell
+composer.phar create-project -sdev --dev --ignore-platform --no-install \
+honeybee/honeybee-agavi-cmf-demo <your-honeybee-project-dir>
+```
+You will be prompted for for some information to configure the VM.
+
+* In order to launch the VM your application must be made available on Github. When the repository is ready at the location you specified in the `create-project` command you can build the VM as follows:
+```shell
+cd honeybee-agavi-cmf-demo/dev/box/
+vagrant up # this will take a while, time to grab a coffee
+```
+
+* Now you can install your application within the box:
+```shell
+vagrant ssh
+cd /srv/www/honeybee-agavi-cmf-demo.local/
+make install
+```
+
+In the end you'll be prompted for some infos. Here are some answers, that are suitable for development:
+
+* Base-url: `https://honeybee-agavi-cmf-demo.local/`
+* Environment: `development`
+* Enable testing: `y`
+
+#### Mounting the source
+
+* MAC:
+    * In the Finder's menubar select: `Connect to Server`
+    * then enter the following address: `nfs://honeybee-agavi-cmf-demo.local/srv/www/`
+* Ubuntu:
+```shell
+mount honeybee-agavi-cmf-demo.local:/srv/www/ /home/${USER}/projects/honeybee-agavi-cmf-demo
+```
+
+When successfully setup the application should be reachable at: https://honeybee-agavi-cmf-demo.local/
+and is ready for development. 
+
+Run the migrations to complete the setup:
+```shell
+make migrate-all
+```
+
+#### Creating the first system account user/admin
+
+The first user within the system must be created via command line using:
+```shell
+make user
+```
+
+This will give an output similar to:
+```
+Please set a password for the created account at: https://honeybee-agavi-cmf-demo.local/honeybee/system_account/user/password?token=c469090bf62c4d21444cd0a83171b1429a11ad9b
+Via CLI use the following: bin/cli honeybee.system_account.user.password '-token' 'c469090bf62c4d21444cd0a83171b1429a11ad9b'
+```
+
+Either copy the displayed url and open it in a browser or run the displayed cli command. Then follow through the instructions in order to set the password for the user, that you just created.
+
+#### Controlling system services
+
+The following services are running on the cms devbox and are controlled via systemd:
+
+* couchdb
+    * http-endpoint: http://honeybee-agavi-cmf-demo.local:5984
+    * web-client: http://honeybee-agavi-cmf-demo.local:5984/_utils
+* elasticsearch
+    * http-endpoint: http://honeybee-agavi-cmf-demo.local:9200
+    * web-client: http://honeybee-agavi-cmf-demo.local:9200/_plugin/head
+* converjon
+    * http-endpoint: https://honeybee-agavi-cmf-demo.local/converjon
+    * web-status: https://honeybee-agavi-cmf-demo.local/converjon/status
+* nginx
+    * cms http-endpoint: https://honeybee-agavi-cmf-demo.local/
+* php-fpm
+
+In order to start/stop services or get the status use the corresponding sudo command within the devbox, e.g.:
+```shell
+sudo service couchdb status|start|stop|restart
+```
+
+#### Turning the VM on/off
+
+Whenever possible stop the box with:
+```shell
+vagrant suspend
+```
+and wake it up again using:
+```shell
+vagrant resume
+```
+This will send the box asleep, instead of completely shutting it off and thus runs faster.
+The box's network interfaces are not reconfigured using suspend/resume though.
+For this the virtual machine needs to be completely rebooted, which can be done by calling:
+```shell
+vagrant reload # is the same as: vagrant halt && vagrant up
+```
+
+In order to apply new infrastructure changes to the box run:
+```shell
+vagrant provision
+```
---
+++ etc/composer/ScriptHandler.php
@@ -0,0 +1,160 @@
+<?php
+
+use Composer\Script\Event;
+
+class ScriptHandler
+{
+    const DEFAULT_HOST_NAME = 'honeybee-agavi-cmf-demo.local';
+    const DEFAULT_REPOSITORY_NAME = 'honeybee/honeybee-agavi-cmf-demo';
+    const DEFAULT_PROJECT_NAME = 'honeybee-agavi-cmf-demo';
+
+    public static function postRootPackageInstall(Event $event)
+    {
+        $project_path = realpath(__DIR__ . '/../../');
+
+        $io = $event->getIO();
+        $io->write('');
+        $io->write('<fg=green;options=bold>Welcome to the Honeybee-Agavi CMF Project Creator</>');
+        $io->write('<info>--------------------------------------------------------------------------------------------');
+        $io->write('This package can be configured for local development or can be configured');
+        $io->write('for easy installation on a preconfigured vagrant virtual machine (VM).</>');
+        $io->write('');
+        $io->write('Your project will now be configured for version control.');
+        $vendor_package = $io->askAndValidate(
+            '<options=bold>Please provide a Github <user>/<repository> name: </>',
+            function($input) {
+                if (!preg_match('#^[\w\.\-]+/[\w\.\-]+$#', $input)) {
+                    throw new Exception('Invalid repository name. Format should be <user>/<repository>');
+                } else {
+                    return $input;
+                }
+            },
+            5
+        );
+        $package_name = substr($vendor_package, strpos($vendor_package, '/') + 1);
+
+        $io->write('');
+        $io->write('This project can be automatically provisoned on a preconfigured virtual machine.');
+        $configure_vm = $io->askConfirmation('<options=bold>Would you like to configure a VM? [y,N]: </>', false);
+        if (true === $configure_vm) {
+            $default_url = 'https://' . $package_name . '.local';
+            $io->write('');
+            $io->write('The project will be prepared in the VM at the given hostname.');
+            $hostname = $io->askAndValidate(
+                '<options=bold>Please provide a valid HTTPS hostname [' . $default_url . ']: </>',
+                function($input) use($default_url) {
+                    if (is_null($input)) {
+                        return parse_url($default_url, PHP_URL_HOST);
+                    } elseif (!filter_var($input, FILTER_VALIDATE_URL) || !preg_match('#^https.+#', $input)) {
+                        throw new Exception('Hostname provided should be a valid HTTPS hostname');
+                    } else {
+                        return parse_url($input, PHP_URL_HOST);
+                    }
+                },
+                5
+            );
+        }
+
+        $io->write('');
+        $io->write('<fg=yellow>');
+        $io->write("                      // \ ");
+        $io->write("                      \\\\_/ // ");
+        $io->write("    ''-.._.-''-.._.. -(||)(')");
+        $io->write("                      '''");
+        $io->write('Please wait while your project is configured...</>');
+        $io->write(PHP_EOL);
+
+        // Replace repository and package name
+        if (isset($hostname)) {
+            self::replaceStringInFiles(self::DEFAULT_HOST_NAME, $hostname, $project_path);
+        }
+        self::replaceStringInFiles(self::DEFAULT_REPOSITORY_NAME, $vendor_package, $project_path);
+        self::replaceStringInFiles(self::DEFAULT_PROJECT_NAME, $package_name, $project_path);
+
+        // Remove .git files if present
+        // self::deleteGitFiles($project_path . DIRECTORY_SEPARATOR . '.git');
+
+        $io->write('<fg=green;options=bold>Your project configuration is complete.</>');
+        $io->write('<info>--------------------------------------------------------------------------------------------');
+
+        if (true === $configure_vm) {
+            $io->write('<fg=green;options=reverse>Before launching the VM you must commit and push this repository to Github.</>');
+            $io->write('');
+            $io->write('Please execute the following git commands as detailed here:');
+            $io->write('<options=underscore>https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line</>');
+            $io->write('');
+            $io->write('<options=bold>git init</>');
+            $io->write('<options=bold>git add .</>');
+            $io->write('<options=bold>git commit -m "Initialising project"</>');
+            $io->write('<options=bold>git remote add origin git@github.com:' . $vendor_package . '.git</>');
+            $io->write('<options=bold>git push origin master</>');
+            $io->write('');
+            $io->write('When the repository is ready, you can launch the VM by executing the following commands:');
+            $io->write('');
+            $io->write('<options=bold>cd ' . $project_path . '/dev/box</>');
+            $io->write('<options=bold>vagrant up</> <comment># this may take a while, time to grab a coffee</comment>');
+            $io->write('');
+            $io->write('Once the VM is up and running you can complete installation using the following commands:');
+            $io->write('');
+            $io->write('<options=bold>vagrant ssh</>');
+            $io->write('<options=bold>cd /srv/www/' . $hostname . '</>');
+            $io->write('<options=bold>make install</>');
+        } else {
+            $io->write('You can now install the application by executing the following command:');
+            $io->write('');
+            $io->write('<options=bold>make install</>');
+            $io->write('');
+            $io->write('Don\'t forget to setup your git repository as detailed here:');
+            $io->write('<options=underscore>https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line</>');
+        }
+
+        $io->write('');
+        $io->write('Further Honeybee information and support can be found here:');
+        $io->write('Installation documentation: <options=underscore>https://github.com/honeybee/honeybee-agavi-cmf-demo</>');
+        $io->write('Cookbook & demo project: <options=underscore>https://github.com/honeybee/honeybee-agavi-cmf-demo/cookbook</>');
+        $io->write('IRC support and feedback: <options=underscore>irc://irc.freenode.org/honeybee</>');
+        $io->write('--------------------------------------------------------------------------------------------</info>');
+        $io->write('<fg=green;options=bold>Thank you for using Honeybee.</>');
+        $io->write('');
+    }
+
+    protected static function replaceStringInFiles($search, $replacement, $path, array $exclude_paths = array())
+    {
+        $objects = new RecursiveIteratorIterator(
+            new RecursiveCallbackFilterIterator(
+                new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
+                function ($current) use ($path, $exclude_paths) {
+                    if ($current->isDir()) {
+                       foreach ($exclude_paths as $exclusion) {
+                            $real_exclusion = $path . DIRECTORY_SEPARATOR . $exclusion;
+                            if ($current->getRealPath() == $real_exclusion) {
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                }
+            ),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        foreach ($objects as $name => $object) {
+            if ($object->isFile() && is_writable($object->getRealPath())) {
+                $file_contents = file_get_contents($object->getRealPath());
+                $file_contents = str_replace($search, $replacement, $file_contents);
+                file_put_contents($object->getRealPath(), $file_contents);
+            }
+        }
+    }
+
+    protected static function deleteGitFiles($path)
+    {
+        if (is_writable($path)) {
+            $files = array_diff(scandir($path), array('.','..'));
+            foreach ($files as $file) {
+                (is_dir("$path/$file")) ? self::deleteGitFiles("$path/$file") : unlink("$path/$file");
+            }
+            rmdir($path);
+        }
+    }
+}
